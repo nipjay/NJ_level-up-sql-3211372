6dcfe330122f286a4e3b7f43c4abc2d06cd0c056
nipjay <147911486+nipjay@users.noreply.github.com>
2023-10-17 17:45:43 +0000

    modified:   01_restaurant/challenges/01_08_findreservation_ch.sql

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,18 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT 
+  R.ReservationID,
+  R.CustomerID,
+  FirstName,
+  LastName,
+  Date,
+  PartySize
+FROM
+  Reservations 'R'
+INNER JOIN
+  Customers 'C'
+ON R.CustomerID = C.CustomerID
+WHERE
+  (FirstName LIKE 'Ste%' OR LastName LIKE 'Ste%') AND Date LIKE '2022-06-14%';
