28fbcefd67dc40da534f096377dab42f97fe1bd0
nipjay <147911486+nipjay@users.noreply.github.com>
2023-10-17 19:53:06 +0000

    modified:   01_restaurant/challenges/01_12_bestcustomers_ch.sql

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,16 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT
+  Count(O.CustomerID) NumberofOrders,  
+  C.CustomerID,
+  C.FirstName,
+  C.LastName,
+  c.Email
+FROM
+  Orders O
+INNER JOIN Customers C
+ON O.CustomerID = C.CustomerID
+GROUP BY C.CustomerID
+ORDER BY NumberofOrders DESC
