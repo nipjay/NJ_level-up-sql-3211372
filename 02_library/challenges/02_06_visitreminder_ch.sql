de9d355d238172f80a5390a330cf1678f2e330cc
nipjay <147911486+nipjay@users.noreply.github.com>
2023-10-18 11:41:15 +0000

    modified:   02_library/challenges/02_06_visitreminder_ch.sql

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,13 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT 
+  COUNT(L.LoanID) NumberofBooks,
+  L.PatronID,
+  P.FirstName,
+  P.Email
+FROM Loans L
+JOIN Patrons P 
+ON L.PatronID = P.PatronID
+GROUP BY L.PatronID
+ORDER BY NumberofBooks 
