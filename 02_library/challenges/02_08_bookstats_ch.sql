866d6e6ed4f80af6af83c624698102b824ad6b35
nipjay <147911486+nipjay@users.noreply.github.com>
2023-10-18 17:56:42 +0000

    modified:   02_library/challenges/02_08_bookstats_ch.sql

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,20 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+SELECT 
+  Published PublishedYear,
+  count(DISTINCT(Title)) NumberofTitles,
+  GROUP_CONCAT(DISTINCT(Title))
+FROM Books
+GROUP BY Published
+ORDER BY NumberofTitles DESC;
+
+SELECT
+  count(L.BookID) NumberofLoans,
+  Title
+FROM Loans L 
+JOIN Books B 
+ON L.BookID = B.BookID
+GROUP BY B.Title
+ORDER BY NumberofLoans DESC;
